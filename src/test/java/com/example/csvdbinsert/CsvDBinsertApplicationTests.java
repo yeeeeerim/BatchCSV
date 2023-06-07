8894377db2a2751cf7206c03bd83f810f615026f
yeeeeerim <gyr3904@gmail.com>
2023-06-07 21:28:11 +0900

    CONFIG: 프로젝트 세팅

---
+++ src/test/java/com/example/csvdbinsert/CsvDBinsertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csvdbinsert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvDBinsertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
